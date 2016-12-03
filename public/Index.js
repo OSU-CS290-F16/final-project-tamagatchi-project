156220e31147e6cd5b863ca3f33209fc75c16de8
Nick Bradford <bradforn@oregonstate.edu>
2016-12-03 15:33:37 -0800

    lots of stuff
    lots of stuff

--- public/Index.js
+++ public/Index.js
@@ -2,3 +2,12 @@ function startGame(imgName) {
     image = document.getElementById('shell');
     image.src = imgName;
 }
+
+// window.addEventListener('DOMContentLoaded', function (event) {
+//
+//
+//
+//   var start = document.getElementById('shell');
+//   start.addEventListener('click', startGame);
+//
+// });
