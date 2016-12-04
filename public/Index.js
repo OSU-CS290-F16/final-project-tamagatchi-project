d7b9217938e3dec600b2dbbc5a549018115e2158
Nick Bradford <bradforn@oregonstate.edu>
2016-12-04 03:51:33 -0800

    seting it up

--- public/Index.js
+++ public/Index.js
@@ -1,8 +1,10 @@
 var intro = true;
-var age = 1;
+var sick = false;
+var age = 0;
 var hunger = 0;
 var happy =0;
 var weight = 0;
+var poo = 0;
 var name = "Deutch";
 var training = 0;
 var dir = '';
@@ -43,13 +45,13 @@ function showButtons(){
      if(intro === true){
        intro = false;
        hideButtons();
+       age = age + 1;
        image.src = "./Characters/Deutch/Age-0/Deutch-Egg.gif";
 
        setTimeout(function(){
          image.src = "./Characters/Deutch/Age-1/Deutch-Age-1-Normal.gif";
          showButtons();
        }, 6000);
-
 
      }
      else{
@@ -63,12 +65,20 @@ function showButtons(){
   function statsFunc(){
     hideButtons();
     image.src = "./Settings/Stats.png";
+    //unhide hearts in hunger and happy places accordingly
     image.addEventListener("click", function(){
       hideButtons();
       image.src = "./Settings/Training.png";
+      //hide hunger and happy hearts
+      // unhide boxes according to training
       image.addEventListener("click", function(){
         hideButtons();
         image.src = "./Settings/Age-Weight-Name.png";
+        //hide boxes
+        //show age icon and swap src if age above 1
+        //unhide name
+        //unhide age and change if above 1
+        //unhide weight and change accordingly
         image.addEventListener("click", function(){
           hideButtons();
           dir = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-" + action + ".gif";
@@ -78,6 +88,11 @@ function showButtons(){
       });
     });
   }
+
+  //function timer(){
+    //hunger = hunger - 1;
+//}
+
 
 
 
@@ -90,6 +105,9 @@ function showButtons(){
 
     if(hunger < 5) {
       hunger = hunger + 1;
+      poo = poo + .25;
+      training = training +.25;
+      weight = weight + .5
     }
   }
 
@@ -99,9 +117,23 @@ function showButtons(){
     setTimeout(function(){
       back();
     }, 3500);
+    if(poo < 1){
+      happy = happy + .5;
+      poo = 0;
+      training = training + .25;
+      weight = weight - .25;
+    }
   }
 
   function playFunc(){
+    dir0 = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-Play.gif";
+    image.src = dir0;
+    setTimeout(function(){
+      back();
+    }, 4500);
+    happy = happy +.5;
+    training = training -.125;
+    weight = weight -.25;
 
   }
 
@@ -110,21 +142,45 @@ function showButtons(){
   }
 
   function goodFunc(){
-
+    dir0 = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-Happy.gif";
+    image.src = dir0;
+    setTimeout(function(){
+      back();
+    }, 3500);
+    happy = happy + 1;
   }
 
   function sickFunc(){
-
+    dir0 = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-Happy.gif";
+    image.src = dir0;
+    setTimeout(function(){
+      back();
+    }, 3500);
+    sick = false;
   }
 
   function sleepFunc(){
-
+    dir0 = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-Sleep.gif";
+    image.src = dir0;
+    setTimeout(function(){
+      back();
+    }, 3500);
+    if(training < (age*age)){//if it sleeps for so long // set a time if time < '#'
+      age = age + 1;
+    }
   }
 
   function badFunc(){
+    dir0 = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-Bad.gif";
+    image.src = dir0;
+    setTimeout(function(){
+      back();
+    }, 3500);
+    happy = happy - 1;
+    training = training + 1;
 
   }
 
   function attentionFunc(){
-
+//idk what to put here
   }
