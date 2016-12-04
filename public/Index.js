c185c1a2522836df22fa448b298b2d8c6e450c2e
Nick Bradford <bradforn@oregonstate.edu>
2016-12-03 20:30:51 -0800

    stuff
    stuff

--- public/Index.js
+++ public/Index.js
@@ -1,30 +1,44 @@
 var intro = true;
-var age = 0;
+var age = 1;
 var hunger = 0;
 var happy =0;
 var weight = 0;
 var name = "Deutch";
 var training = 0;
+var dir = '';
+var action = "Normal";
+
+
+
 //var Gender;
 document.getElementById("caseShell").addEventListener("click", screenClick);
-
-
-
+document.getElementById("stats").addEventListener("click", status);
 
 
  function screenClick() {
    console.log("It worked");
      image = document.getElementById('caseShell');
 
-     if(intro = true){
+     if(intro === true){
        intro = false;
        image.src = "./Characters/Deutch/Age-0/Deutch-Egg.gif";
-       gif.finish('end', function (){
+
+       setTimeout(function(){
          image.src = "./Characters/Deutch/Age-1/Deutch-Age-1-Normal.gif";
-       });
+       }, 6000);
+
      }
-       image.src = imgName;
+     else{
+        dir = "./Characters/" + name + "/Age-" + age + "/" + name + "-Age-" + age + "-" + action + ".gif";
+        image.src = dir;
+     }
+
 };
+
+  function status(){
+
+    image.src = "./Settings/Stats.png";
+  }
 
 //function eventListen() {
 //  console.log("It worked");
